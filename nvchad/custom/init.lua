fc9cb229545ab58aed5c734dcf7754d1c6347048
soobeenpark <soobeenpark.dev@gmail.com>
2025-01-19 18:40:57 -0800

    Add nvchad compile with C-g for cpp

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -2,6 +2,12 @@
 vim.opt.colorcolumn = '100'
 
 -- enable delta line numbers
-vim.wo.relativenumber = true
+vim.opt.relativenumber = true
 
-
+-- for competitive programming (c++)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+      vim.api.nvim_buf_set_keymap(0,"n","<C-g>",":split<CR>:te g++-14 -std=c++14 -Wshadow -Wall -o out % -g -D_GLIBCXX_DEBUG && ./out < in<CR>i", {})
+  end
+})
