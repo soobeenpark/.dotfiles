28bb8bcfb483a0904a290ce44bc567f3c49dee9a
soobeenpark <soobeenpark.dev@gmail.com>
2025-01-20 16:11:20 -0800

    Add nvchad easy pasting for cpp

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -11,4 +11,13 @@ vim.api.nvim_create_autocmd("FileType", {
       vim.api.nvim_buf_set_keymap(0,"n","<C-g>",":split<CR>:te g++-14 -std=c++14 -Wshadow -Wall -o out % -g -D_GLIBCXX_DEBUG && ./out < in<CR>i", {})
   end
 })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cpp",
+  callback = function()
+    vim.keymap.set('n', '<C-i>', function()
+      os.execute('pbpaste > in')
+    end, { noremap = true, buffer = true }
+    )
+  end
+})
 
